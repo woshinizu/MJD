65dbc505567f7352d47e48e25d4642e9859c2b36
yangyang <yangyang@blackpai.com>
2019-12-09 22:39:27 +0800

    星期一

--- src/main.js
+++ src/main.js
@@ -2,47 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-<<<<<<< HEAD
-import { 
-  Search,
-  Button,
-  Icon,
-  Tab,
-  Tabs,
-  RadioGroup,
-  Radio,
-  Popup,
-  Cell,
-  Stepper,
-  Image,
-  Swipe,
-  SwipeItem,
-  Divider,
-  Checkbox
-} from 'vant';
+import { Search, Button, Icon, Tab, Tabs, Swipe, SwipeItem, GoodsAction, GoodsActionIcon, GoodsActionButton, NoticeBar, TreeSelect, Grid, GridItem, Image, Divider, Checkbox, Stepper ,Cell,Popup} from 'vant';
 import '@/less/commen.less' // 引入公用样式
-Vue.use(Search)
-  .use(Button)
-  .use(Icon)
-  .use(Tab)
-  .use(Tabs)
-  .use(RadioGroup)
-  .use(Radio)
-  .use(Popup)
-  .use(Cell)
-  .use(Stepper)
-  .use(Image)
-  .use(Swipe)
-  .use(SwipeItem)
-  .use(Divider)
-  .use(Checkbox)
-=======
-import { Search, Button, Icon, Tab, Tabs, Swipe, SwipeItem, GoodsAction, GoodsActionIcon, GoodsActionButton, NoticeBar, TreeSelect, Grid, GridItem, Image } from 'vant';
-import '@/less/commen.less' // 引入公用样式
-Vue.use(Search).use(Button).use(Icon).use(Tab).use(Tabs).use(Swipe).use(SwipeItem).use(GoodsAction).use(GoodsActionIcon).use(GoodsActionButton).use(NoticeBar).use(TreeSelect).use(Grid).use(GridItem).use(Image)
+Vue.use(Search).use(Button).use(Icon).use(Tab).use(Tabs).use(Swipe).use(SwipeItem).use(GoodsAction).use(GoodsActionIcon).use(GoodsActionButton).use(NoticeBar).use(TreeSelect).use(Grid).use(GridItem).use(Image).use(Divider).use(Checkbox).use(Stepper).use(Cell).use(Popup)
 
 
->>>>>>> f9785abbd49fbcdba34f33bc2519a987fff27a6a
 
 Vue.config.productionTip = false
 
