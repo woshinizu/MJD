27118f95c3ceb6f7a6bae6be2c2d069ea48d2ae2
shuai shang <1652570948@qq.com>
2019-12-09 22:28:43 +0800

    s

--- src/components/found/found.vue
+++ src/components/found/found.vue
@@ -55,8 +55,7 @@ export default {
     }
 }
 </script>
-<style lang="less" scoped
->
+<style lang="less" scoped>
 .header{
     width:100%;
     height:15vw;
--- src/components/home/header/searchList.vue
+++ src/components/home/header/searchList.vue
@@ -7,49 +7,55 @@
         </div>
       </router-link>
       <div class="Search">
-        <van-search
-          v-model="value"
-          :placeholder="value"
-          show-action
-          white="100vw"
-          shape="round"
-        >
-          <div slot="action" >搜索</div>
+        <van-search v-model="value" :placeholder="value" show-action white="100vw" shape="round">
+          <div slot="action">搜索</div>
         </van-search>
       </div>
     </div>
+    <div class="searchListBox">
+      <span class="span1" @click="switchs">商铺</span>
+      <span class="span2" @click="switchs">旗舰店</span>
+    </div>
     <div class="shopBox">
-        <div v-if='flag'>
-            <shops></shops>
-        </div>
-         <div v-else>
-             <noshops></noshops>
-         </div>
+      <div v-if="flag">
+        <shops></shops>
+      </div>
+      <div v-else>
+        <noshops></noshops>
+      </div>
     </div>
   </div>
 </template>
 <script>
 // @ is an alias to /src
-import shops from '@/components/home/header/searchfen/shops.vue'
-import noshops from '@/components/home/header/searchfen/noshops.vue'
+import shops from "@/components/home/header/searchfen/shops.vue";
+import noshops from "@/components/home/header/searchfen/noshops.vue";
 export default {
   name: "searchList",
   data() {
     return {
-        value:this.$route.query.keywords,
-        flag:true
+      value: this.$route.query.keywords,
+    //   flag: this.$router.query.flag ,
     };
   },
   created() {
-      console.log(this.$route)
+    console.log(this.$route);
+    
+  },
+  computed: {
+      flag(){
+          return this.$route.query.flag == 'true' 
+      }
   },
   components: {
-      shops,noshops
-      
+    shops,
+    noshops
   },
   methods: {
-      
-  },
+       switchs(){
+
+       }
+  }
 };
 </script>
 <style lang="less">
@@ -67,10 +73,27 @@ export default {
     }
   }
 }
-.shopBox{
-    position: absolute;
-    background: rgb(209, 209, 209,0.1);
-    height: 100%;
-    
+.shopBox {
+  position: absolute;
+  background: rgb(209, 209, 209, 0.1);
+  height: 100%;
+}
+.searchListBox{
+    text-align: center;
+    margin: 2vh auto;
+    background: #ccc;
+    width: 30vw;
+    height: 4vh;
+    line-height: 4vh;
+    border-radius: 5px;
+    .span1{
+        margin-right: 4vw;
+    }
+    .span2{
+
+    }
+    >span:active{
+        color: red;
+    }
 }
 </style>
