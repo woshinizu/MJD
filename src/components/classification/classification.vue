9a6c83403994b3a7e5c851c580adfe59f1b7974b
liumingxiapiao <53509065+liumingxiapiao@users.noreply.github.com>
2019-12-09 10:09:46 +0800

    first

--- src/components/classification/classification.vue
+++ src/components/classification/classification.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
-        adasdasd
+     <van-search
+  v-model="value"
+  placeholder="请输入搜索关键词"
+  shape="round"
+  @search="onSearch"
+>
+</van-search>
     </div>
 </template>
 <script>
+ 
 // @ is an alias to /src
 export default {
     name: 'classification',
